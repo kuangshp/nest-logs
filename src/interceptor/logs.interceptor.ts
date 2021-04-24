17782939d0e6a039d6db21b0a05dd58db75f4f1b
kuangshp <kuangshp@126.com>
2021-04-25 07:56:54 +0800

    feat: 新增显示ip地址

--- src/interceptor/logs.interceptor.ts
+++ src/interceptor/logs.interceptor.ts
@@ -10,13 +10,13 @@ export class LogsInterceptor implements NestInterceptor {
     const request: Request = context.switchToHttp().getRequest();
     const isPrintLog = Reflect.getMetadata(LOG_KEY, context.getHandler()) || Reflect.getMetadata(LOG_KEY, context.getClass());
     const fileName = Reflect.getMetadata(LOG_MESSAGE, context.getHandler()) || Reflect.getMetadata(LOG_MESSAGE, context.getClass());
-    console.log(isPrintLog, fileName, '?????')
     if (isPrintLog) {
       const logStr = `
   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
   当前类名: ${context.getClass()['name']}
   当前方法名: ${context.getHandler()['name']}
   当前请求: ${request.method} - ${request.url}
+  IP: ${request.ip}
   当前Params参数: ${JSON.stringify(request.params, null, 2)}
   当前Query参数: ${JSON.stringify(request.query, null, 2)}
   当前请求体: ${JSON.stringify(request.body, null, 2)}
